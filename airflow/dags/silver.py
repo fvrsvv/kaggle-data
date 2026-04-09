25e95dd863a35cb6f16522797fac8f10f37a171a
fvrsvv <lizafavarisova@gmaill.com>
2026-04-09 13:40:49 +0300

    add spark container, error: Failed to connect to master spark-master:7077

--- airflow/dags/silver.py
+++ airflow/dags/silver.py
@@ -1,81 +1,32 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, current_date, lit, lower, trim
-from minio import Minio
-from minio.error import S3Error
-import logging
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-def raw_to_silver_job():
-    spark = SparkSession.builder \
-        .appName("raw_to_silver_job_salary_iceberg") \
-        .master("local[*]") \
-        .config("spark.jars.packages", 
-                "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1,"      
-                "org.apache.hadoop:hadoop-aws:3.3.4,"
-                "com.amazonaws:aws-java-sdk-bundle:1.12.262,"
-                "software.amazon.awssdk:bundle:2.32.29,"                     
-                "software.amazon.awssdk:url-connection-client:2.32.29") \
-        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkCatalog") \
-        .config("spark.sql.catalog.spark_catalog.type", "hadoop") \
-        .config("spark.sql.catalog.spark_catalog.warehouse", "s3a://silver/") \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
-        .config("spark.hadoop.fs.s3a.access.key", "minio") \
-        .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
-        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-        .config("spark.hadoop.fs.s3a.endpoint.region", "us-east-1") \
-        .getOrCreate()
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+}
 
-    try:
-        df = spark.read.option("multiline", "true").json("s3a://raw/job_salary_prediction_dataset.json")
-        logging.info(f"Прочитано строк из raw: {df.count()}")
-
-        df_clean = (df
-            .dropDuplicates()
-            .filter(col("salary").isNotNull() & (col("salary") > 0))
-            .withColumn("job_title", lower(trim(col("job_title"))))
-            .withColumn("industry", lower(trim(col("industry"))))
-            .withColumn("education_level", lower(trim(col("education_level"))))
-            .withColumn("ingestion_date", current_date())
-            .withColumn("load_timestamp", lit(datetime.now()))
-            .withColumn("source", lit("kaggle"))
-        )
-
-        silver_table = "spark_catalog.silver.job_salary_prediction"
-
-        df_clean.writeTo(silver_table) \
-            .tableProperty("format-version", "2") \
-            .tableProperty("write.parquet.compression-codec", "zstd") \
-            .partitionedBy("ingestion_date") \
-            .createOrReplace()   
-
-        logging.info(f"Успешно записано в Iceberg таблицу {silver_table}: {df_clean.count()} строк")
-        df_clean.printSchema()
-
-        spark.sql(f"SELECT * FROM {silver_table} LIMIT 5").show()
-
-    except Exception as e:
-        logging.error(f"Ошибка при трансформации в Silver (Iceberg): {e}")
-        raise
-    finally:
-        spark.stop()
-
-dag = DAG(
-    dag_id="raw_to_silver_iceberg",
-    schedule_interval=None,
+with DAG(
+    dag_id="raw_to_silver_spark",
+    default_args=default_args,
     schedule=None,
+    start_date=datetime(2025, 1, 1),
     catchup=False,
-    tags=["delta", "to silver"],
-)
+    tags=["spark", "minio", "iceberg"],
+) as dag:
 
-raw_to_silver_task = PythonOperator(
-    task_id="raw_to_silver_iceberg",
-    python_callable=raw_to_silver_job,
-    dag=dag,
-)
-
-(raw_to_silver_task)
+    bronze_to_silver = SparkSubmitOperator(
+        task_id="bronze_to_silver_etl",
+        application="/opt/spark_jobs/etl_raw_to_silver.py",   # путь внутри Airflow контейнера
+        conn_id="spark_default",                              # берётся из AIRFLOW_CONN_...
+        deploy_mode="client",
+        conf={
+            "spark.executor.memory": "2g",
+            "spark.executor.cores": "2",
+            "spark.driver.memory": "1g",
+        },
+        verbose=True,
+        dag=dag,
+    )
---
+++ spark_jobs/etl_raw_to_silver.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lower, trim, current_date, current_timestamp, lit
+import sys
+
+spark = SparkSession.builder \
+    .appName("Raw_to_Silver_Job") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.access.key", "minio") \
+    .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .getOrCreate()
+
+df = spark.read.json("s3a://raw/job_salary_prediction_dataset.json")
+
+df_clean = (df
+    .dropDuplicates(["job_title", "industry", "education_level", "salary"])
+    .filter((col("salary").isNotNull()) & (col("salary") > 0))
+    .withColumn("job_title", lower(trim(col("job_title"))))
+    .withColumn("industry", lower(trim(col("industry"))))
+    .withColumn("education_level", lower(trim(col("education_level"))))
+    .withColumn("ingestion_date", current_date())
+    .withColumn("load_timestamp", current_timestamp())
+    .withColumn("source", lit("kaggle"))
+)
+
+df_clean.write \
+    .mode("overwrite") \
+    .parquet("s3a://silver/job_salary_prediction")
+
+print("✅ Job completed successfully!")
+spark.stop()
